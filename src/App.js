441c7962d2457b2fc06e8bf65b017b51bfc3c12f
luca98-developer <lucacaimi@yahoo.com>
2023-03-14 12:01:32 +0000

    try

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { selectCount, increment, decrement } from "./features/hospitality/hospitalitySlice";
+
+import Data from "./component/Data";
 
 export default function App() {
-  const count = useSelector(selectCount);
-  const dispatch = useDispatch();
-
   return (
-    <div>
-      <div >
-        <button onClick={() => dispatch(increment())}>
-          +
-        </button>
-        <button onClick={() => dispatch(decrement())}>
-          -
-        </button>
-        <span >{count}</span>
-      </div>
-      
-    </div>
+    <>
+      <Data />
+    </>
   );
 }
+//employer name
+//location
+//type of business
+//number
+//emails
+//about...
