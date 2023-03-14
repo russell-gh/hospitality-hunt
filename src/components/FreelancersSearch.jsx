b2da506a7a92871b26ec1aa768b1c8a07368131a
Bernie Lam <bernie@Berniede-MBP.cust.communityfibre.co.uk>
2023-03-14 00:59:51 +0000

    half done

--- src/components/FreelancersSearch.jsx
+++ src/components/FreelancersSearch.jsx
@@ -1,40 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { selectCount } from "../features/hospitality/hospitalitySlice";
 
 const FreelancersSearch = () => {
   const users = useSelector(selectCount);
-  console.log(users);
+  const [userInput, setUserInput] = useState("");
+  const [userSelect, setUserSelect] = useState("");
 
   if (!users) {
     return <p>Loading...</p>;
   }
 
+  let filtered = users;
+  if (userSelect === "Location") {
+    filtered = users.filter((users) => {
+      return (
+        users.location.toLowerCase().includes(userInput) ||
+        users.location.toUpperCase().includes(userInput)
+      );
+    });
+  } else if (userSelect === "Job-type") {
+    filtered = users.filter((users) => {
+      return (
+        users.business_type.toLowerCase().includes(userInput) ||
+        users.business_type.toUpperCase().includes(userInput)
+      );
+    });
+  } else {
+    filtered = users.filter((users) => {
+      return (
+        users.position.toLowerCase().includes(userInput) ||
+        users.position.toUpperCase().includes(userInput)
+      );
+    });
+  }
+
   return (
     <>
-      <div>
-        <input
-          // onInput={(e) => {
-          //   setUserInput(e, target.value);
-          // }}
-          type="text"
-          placeholder="Search for a job"
-        ></input>
+      {/* <div>
+        <label>I'm looking for:</label>
         <select>
-          <option>Job type</option>
-          <option>Skills</option>
-          <option>Location</option>
+          <option>Job</option>
+          <option>Freelancer</option>
+        </select>
+      </div> */}
+
+      <div>
+        <label>Search by:</label>
+        <select
+          onChange={(e) => {
+            setUserSelect(e.target.value);
+          }}
+        >
+          <option value="Location">Location</option>
+          <option value="Job-type">Job type</option>
+          <option value="Position">Position</option>
         </select>
       </div>
 
       <div>
-        {/* {users.map((item) => (
-          <>
-            <p>{item.type}</p>
-          </>
-        ))} */}
+        <input
+          onInput={(e) => {
+            setUserInput(e.target.value);
+          }}
+          type="text"
+          placeholder="Search for a job"
+        ></input>
+      </div>
 
-        <p>{users}</p>
+      <div>
+        {filtered.map((item) => (
+          <>
+            <div className="eachJob" key={item.ID}>
+              <p>{item.business_name}</p>
+              <p>{item.location}</p>
+              <p>{item.business_type}</p>
+              <p>{item.position}</p>
+              <p>{item.email}</p>
+            </div>
+          </>
+        ))}
       </div>
     </>
   );
--- src/features/hospitality/hospitalitySlice.js
+++ src/features/hospitality/hospitalitySlice.js
@@ -2,56 +2,56 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   users: [
-    {
-      ID: 1,
-      type: "Freelancer",
-      freelancer_name: "Joe Bloggs",
-      location: "TW1 1AH",
-      about: "",
-      role: "server",
-      skills: "bartending, server",
-      email: "joebloggs@email.com",
-    },
-    {
-      ID: 2,
-      type: "Freelancer",
-      freelancer_name: "Luna Estrada",
-      location: "E17 8PQ",
-      about: "",
-      role: "server",
-      skills: "server",
-      email: "lunaestrada@email.com",
-    },
-    {
-      ID: 3,
-      type: "Freelancer",
-      freelancer_name: "Natasha Schmidt",
-      location: "N17 0TG",
-      about: "",
-      role: "server",
-      skills: "bartender, host",
-      email: "natashaschmidt@email.com",
-    },
-    {
-      ID: 4,
-      type: "Freelancer",
-      freelancer_name: "Peter Gordon",
-      location: "SE16 5HL",
-      about: "",
-      role: "bartender",
-      skills: "bartender,manager",
-      email: "petergordon@email.com",
-    },
-    {
-      ID: 5,
-      type: "Freelancer",
-      freelancer_name: "Beatrice Middleton",
-      location: "W2 3NJ",
-      about: "",
-      role: "bartender, server",
-      skills: "bartender,server",
-      email: "beatricemiddleton@email.com",
-    },
+    // {
+    //   ID: 1,
+    //   type: "Freelancer",
+    //   freelancer_name: "Joe Bloggs",
+    //   location: "TW1 1AH",
+    //   about: "",
+    //   role: "server",
+    //   skills: "bartending, server",
+    //   email: "joebloggs@email.com",
+    // },
+    // {
+    //   ID: 2,
+    //   type: "Freelancer",
+    //   freelancer_name: "Luna Estrada",
+    //   location: "E17 8PQ",
+    //   about: "",
+    //   role: "server",
+    //   skills: "server",
+    //   email: "lunaestrada@email.com",
+    // },
+    // {
+    //   ID: 3,
+    //   type: "Freelancer",
+    //   freelancer_name: "Natasha Schmidt",
+    //   location: "N17 0TG",
+    //   about: "",
+    //   role: "server",
+    //   skills: "bartender, host",
+    //   email: "natashaschmidt@email.com",
+    // },
+    // {
+    //   ID: 4,
+    //   type: "Freelancer",
+    //   freelancer_name: "Peter Gordon",
+    //   location: "SE16 5HL",
+    //   about: "",
+    //   role: "bartender",
+    //   skills: "bartender,manager",
+    //   email: "petergordon@email.com",
+    // },
+    // {
+    //   ID: 5,
+    //   type: "Freelancer",
+    //   freelancer_name: "Beatrice Middleton",
+    //   location: "W2 3NJ",
+    //   about: "",
+    //   role: "bartender, server",
+    //   skills: "bartender,server",
+    //   email: "beatricemiddleton@email.com",
+    // },
     {
       ID: 6,
       type: "Business",
@@ -111,13 +111,10 @@ export const hospitalitySlice = createSlice({
   reducers: {},
 });
 
-// export const {  } = hospitalitySlice.actions;
+export const { users } = hospitalitySlice.actions;
 
-export const selectCount = (state) => state.hospitality.value;
+export const selectCount = (state) => state.hospitality.users;
+//----- tell computer what you select
+//----- can see the dev tool to get some hint
 
 export default hospitalitySlice.reducer;
-
-// increment: (state) => {
-//   state.value += 1;
-// }
-// export const { increment, decrement } = hospitalitySlice.actions;
