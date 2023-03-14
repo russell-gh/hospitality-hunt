39054e80f716104ff956d10958ed61bde456154c
Bernie Lam <bernie@Berniede-MBP.cust.communityfibre.co.uk>
2023-03-14 14:58:39 +0000

    testing

--- src/components/SearchForJob.jsx
+++ src/components/SearchForJob.jsx
@@ -1,51 +1,42 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import { selectBusinesses } from "../features/hospitality/hospitalitySlice";
+import { selectJob } from "../features/hospitality/hospitalitySlice";
 
-const FreelancersSearch = () => {
-  const businesses = useSelector(selectBusinesses);
+const SearchForJob = () => {
+  const job = useSelector(selectJob);
   const [userInput, setUserInput] = useState("");
   const [userSelect, setUserSelect] = useState("");
 
-  console.log(businesses);
-  if (!businesses) {
+  if (!job) {
     return <p>Loading...</p>;
   }
 
-  let filtered = businesses;
+  let filtered = job;
   if (userSelect === "Location") {
-    filtered = businesses.filter((users) => {
+    filtered = job.filter((job) => {
       return (
-        users.location.toLowerCase().includes(userInput) ||
-        users.location.toUpperCase().includes(userInput)
+        job.location.toLowerCase().includes(userInput) ||
+        job.location.toUpperCase().includes(userInput)
       );
     });
   } else if (userSelect === "Job-type") {
-    filtered = businesses.filter((users) => {
+    filtered = job.filter((job) => {
       return (
-        users.business_type.toLowerCase().includes(userInput) ||
-        users.business_type.toUpperCase().includes(userInput)
+        job.business_type.toLowerCase().includes(userInput) ||
+        job.business_type.toUpperCase().includes(userInput)
       );
     });
   } else {
-    filtered = businesses.filter((users) => {
+    filtered = job.filter((job) => {
       return (
-        users.position.toLowerCase().includes(userInput) ||
-        users.position.toUpperCase().includes(userInput)
+        job.position.toLowerCase().includes(userInput) ||
+        job.position.toUpperCase().includes(userInput)
       );
     });
   }
 
   return (
     <>
-      {/* <div>
-        <label>I'm looking for:</label>
-        <select>
-          <option>Job</option>
-          <option>Freelancer</option>
-        </select>
-      </div> */}
-
       <div>
         <label>Search by:</label>
         <select
@@ -73,11 +64,10 @@ const FreelancersSearch = () => {
         {filtered.map((item) => (
           <>
             <div className="eachJob" key={item.ID}>
-              <p>{item.business_name}</p>
-              <p>{item.location}</p>
-              <p>{item.business_type}</p>
-              <p>{item.position}</p>
-              <p>{item.email}</p>
+              <h1>{item.business_name}</h1>
+              <p>Location: {item.location}</p>
+              <p>Type: {item.business_type}</p>
+              <p>Position: {item.position}</p>
             </div>
           </>
         ))}
@@ -86,4 +76,4 @@ const FreelancersSearch = () => {
   );
 };
 
-export default FreelancersSearch;
+export default SearchForJob;
