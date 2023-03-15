24d747aa62a604778a6844d72fd5c02d3ece5085
YY199116 <y.yusuf@outlook.com>
2023-03-15 13:29:29 +0200

    added

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 const Navigation = () => {
-    return (<></>);
-}
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Hospitality-Hunt</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Search for Job</Nav.Link>
+            <Nav.Link href="#home">Search for freelancer</Nav.Link>
+            <Nav.Link href="#link">Edit profile</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default Navigation;
--- src/features/hospitality/hospitalitySlice.js
+++ src/features/hospitality/hospitalitySlice.js
@@ -1,10 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  screenMode: 1,
+  screenMode: 2,
   value: 10,
   user: { email: "", password: "" },
-
 
   businesses: [
     {
@@ -110,7 +109,6 @@ const initialState = {
       email: "beatricemiddleton@email.com",
     },
   ],
-
 };
 
 export const hospitalitySlice = createSlice({
@@ -120,6 +118,7 @@ export const hospitalitySlice = createSlice({
   reducers: {
     login: (state, action) => {
       state.user = action.playload;
+      state.screenMode = 3;
     },
 
     increment: (state) => {
@@ -137,17 +136,19 @@ export const hospitalitySlice = createSlice({
     setUserProfile: (state, payload) => {
       state.userProfile = payload;
     },
+
+    setScreenMode: (state, payload) => {
+      state.screenMode = payload;
+    },
   },
 });
-
-
 
 export const { increment, decrement, setUserProfile, SIGNUP, login } =
   hospitalitySlice.actions;
 
 export const selectBusinesses = (state) => state.hospitality.businesses;
 export const selectFreelancers = (state) => state.hospitality.freelancers;
-export const selectScreenMode = (state) => state.hospitality.screenMode
+export const selectScreenMode = (state) => state.hospitality.screenMode;
 //----- tell computer what you select
 //----- can see the dev tool to get some hint
 
