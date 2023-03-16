eb3b7d5799513865ff0f857f4af1f92b64f948db
Bernie Lam <bernie@Berniede-MacBook-Pro.local>
2023-03-16 10:52:20 +0000

    result structure changed and waiting for data create

--- src/components/JobDetails.jsx
+++ src/components/JobDetails.jsx
@@ -1,48 +1,49 @@
 import React from "react";
-import { selectBusinesses } from "../features/hospitality/hospitalitySlice";
+import { selectJobListing } from "../features/hospitality/hospitalitySlice";
 import { useSelector } from "react-redux";
 import "./JobDetails.css";
-import { listingText } from '../language/english';
+import { listingText } from "../language/english";
 
 const JobDetails = () => {
+  const businesses = useSelector(selectJobListing);
+  console.log(businesses);
+  if (!businesses) {
+    return <p>Loading...</p>;
+  }
 
-    const businesses = useSelector(selectBusinesses);
-    console.log(businesses);
-    if (!businesses) {
-        return <p>Loading...</p>;
-    }
+  return businesses.map((business) => {
+    const { business_name, location, business_type, position, email, about } =
+      business;
 
-    return businesses.map((business) => {
-        const { business_name, location, business_type, position, email, about } =
-            business;
+    const bArray = Object.entries(business);
 
-        const bArray = Object.entries(business)
-
-        return bArray.map(item => {
-            return <div style={{ display: 'flex' }}>
-                <p>{listingText[item[0]] ? listingText[item[0]] : item[0]}</p>
-                <p>{item[1]}</p>
-            </div>
-        })
-
-        // return (
-        //     <div className="job-details">
-        //         <h1>Job Details</h1>
-        //         <h2>Type of Role</h2>
-        //         <p>{business_name}</p>
-        //         <h2>Location of Restaurant:</h2>
-        //         <p>{location}</p>
-        //         <h2>Type of Business:</h2>
-        //         <p>{business_type}</p>
-        //         <h2>Type of Role:</h2>
-        //         <p>{position}</p>
-        //         <h2>Contact information:</h2>
-        //         <p>{email}</p>
-        //         <h2>Further information:</h2>
-        //         <p>{about}</p>
-        // //    </div>
-        // );
+    return bArray.map((item) => {
+      return (
+        <div style={{ display: "flex" }}>
+          <p>{listingText[item[0]] ? listingText[item[0]] : item[0]}</p>
+          <p>{item[1]}</p>
+        </div>
+      );
     });
+
+    // return (
+    //     <div className="job-details">
+    //         <h1>Job Details</h1>
+    //         <h2>Type of Role</h2>
+    //         <p>{business_name}</p>
+    //         <h2>Location of Restaurant:</h2>
+    //         <p>{location}</p>
+    //         <h2>Type of Business:</h2>
+    //         <p>{business_type}</p>
+    //         <h2>Type of Role:</h2>
+    //         <p>{position}</p>
+    //         <h2>Contact information:</h2>
+    //         <p>{email}</p>
+    //         <h2>Further information:</h2>
+    //         <p>{about}</p>
+    // //    </div>
+    // );
+  });
 };
 
 export default JobDetails;
