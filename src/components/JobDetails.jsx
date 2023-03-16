cd141018b858808f02aa9ae8207262fd98dc5558
Daniel Smith <danielsmith@Daniels-MacBook-Pro.local>
2023-03-16 16:59:39 +0000

    added button, styling

--- src/components/JobDetails.jsx
+++ src/components/JobDetails.jsx
@@ -16,19 +16,27 @@ const JobDetails = () => {
   const result = jobListings.filter((item) => {
     return item.ID === lastAddedJobId;
   });
+  console.log(result[0]);
 
   const bArray = Object.entries(result[0]);
 
-  return bArray.map((item) => {
-    return (
+  return (
+    <>
+      <h1>Job Details</h1>
       <div className="job-details">
-        <p>
-          {item[0]}: {item[1]}
-        </p>
+        {bArray.map((item) => {
+          return (
+            <p>
+              {item[0]}: {item[1]}
+            </p>
+          );
+        })}
+        <button type="button" className="btn btn-primary btn-lg">
+          APPLY
+        </button>
       </div>
-    );
-
-  });
+    </>
+  );
 };
 
 export default JobDetails;
