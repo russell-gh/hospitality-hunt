1db1e9b506c1ac1f368914b0904f71c9f912485f
Amelia Qualtrough <ameliaqualtrough@gmail.com>
2023-03-20 10:58:31 +0000

    improved webcam feature

--- src/components/react-webcam/WebcamContainer.jsx
+++ src/components/react-webcam/WebcamContainer.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import Webcam from "react-webcam";
+import { useDispatch } from "react-redux";
+import { setUserImage } from "../../features/hospitality/hospitalitySlice";
 
 const videoConstraints = {
-  width: 1280,
-  height: 720,
+  width: 400,
+  height: 300,
   facingMode: "user",
 };
 
 const WebcamContainer = () => {
   const [image, setImage] = useState();
+  const dispatch = useDispatch();
 
   const onImageClick = (image) => {
     setImage(image);
@@ -16,38 +19,47 @@ const WebcamContainer = () => {
 
   const onSave = () => {
     //send to  store
+    console.log(image);
+    dispatch(setUserImage(image));
   };
 
   return (
     <>
-      {image && (
+      <div className="photoContainer">
         <div>
-          <img src={image} alt="You" />{" "}
-          {/* place the confirm on top of the live image */}
-          <button onClick={onSave}>Save</button>
-        </div>
-      )}
-
-      <Webcam
-        audio={false}
-        height={720}
-        screenshotFormat="image/jpeg"
-        width={1280}
-        videoConstraints={videoConstraints}
-      >
-        {({ getScreenshot }) => {
-          return (
-            <button
-              onClick={() => {
-                const imageSrc = getScreenshot();
-                onImageClick(imageSrc);
-              }}
+          {image && (
+            <>
+              <img className="photo" src={image} alt="You" />{" "}
+              {/* place the confirm on top of the live image */}
+              <button onClick={onSave}>Save</button>
+              <button onClick={() => onImageClick(undefined)}>Retake</button>
+            </>
+          )}
+          <div className="webcamBox">
+            <Webcam
+              className="webcam"
+              audio={false}
+              height={300}
+              screenshotFormat="image/jpeg"
+              width={400}
+              videoConstraints={videoConstraints}
             >
-              Capture photo
-            </button>
-          );
-        }}
-      </Webcam>
+              {({ getScreenshot }) => {
+                return (
+                  <button
+                    onClick={() => {
+                      const imageSrc = getScreenshot();
+                      onImageClick(imageSrc);
+                    }}
+                  >
+                    Capture photo
+                  </button>
+                );
+              }}
+            </Webcam>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
