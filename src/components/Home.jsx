f9eb6f2801b8b61565ee6ff8de45aa2b0a5122ac
StuPM <stuartpmcgee@gmail.com>
2023-03-27 14:27:24 +0100

    basic example of gsap added to home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useLayoutEffect, useState } from "react";
 import { setScreenMode } from "../features/hospitality/hospitalitySlice";
 import "./Home.css";
 import { useDispatch, useSelector } from "react-redux";
 import { selectLoggedIn } from "../features/hospitality/hospitalitySlice";
 import hh1 from "../assets/hh1.png";
+import gsap from "gsap";
 
 const Home = () => {
   const dispatch = useDispatch();
   const loggedIn = useSelector(selectLoggedIn);
+  const [go, setGo] = useState(false);
+
   // console.log(loggedIn);
+
+  useLayoutEffect(() => {
+    console.log("Use layout effect.");
+    gsap.fromTo(".logo", { rotation: 0 }, { duration: 2, rotation: 25 });
+  }, [go]);
+
+  console.log(go);
+
   return (
     <>
+      <button
+        onClick={() => {
+          setGo(!go);
+        }}
+      >
+        Go!
+      </button>
       {loggedIn ? (
         <p>You are logged in.</p>
       ) : (
@@ -23,8 +41,8 @@ const Home = () => {
           >
             LOGIN
           </button>
-          <div class="bg image hover-zoom">
-            <img className="w-100 hover-shadow" src={hh1} alt="logo"></img>
+          <div className="bg image hover-zoom">
+            <img className="logo" src={hh1} alt="logo" />
           </div>
           <button
             className="signUp_btn"
