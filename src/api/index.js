ded57c1f514f173f7aa5dccb62fe28ea289f1a0e
Amelia Qualtrough <ameliaqualtrough@gmail.com>
2023-04-13 11:18:49 +0200

    linked logout to back end

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,31 @@
-import axios from 'axios';
+import axios from "axios";
 
 async function api(type, payload) {
-    switch (type) {
-        case "SIGNUP":
-            const { data } = await axios.post('http://localhost:6001/signup', payload)
-            return data;
+  switch (type) {
+    case "SIGNUP":
+      const { data } = await axios.post(
+        "http://localhost:6001/signup",
+        payload
+      );
+      return data;
 
-        default:
-            break;
-    }
+    default:
+      break;
+  }
 
+  switch (type) {
+    case "LOGOUT":
+      const { data } = await axios.delete("http://localhost:6001/logout", {
+        headers: {
+          token:
+            "1681313397261vvImytsMGTdO058p4ZzdfLyBGX2OyktVuy2um47k28HBrjdTv6o4JXqkjk2s8m2gPb0FyLZE0yDwencLZjWG2oJkBjNuCODVMWEsWqwoFYaDgJbgIk8ARwgh1RoeEopZ",
+        },
+      });
+      return data;
+
+    default:
+      break;
+  }
 }
 
 export default api;
