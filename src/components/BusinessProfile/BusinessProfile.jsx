869dff289a4d9c3eb78edffdd832a9cf9554fde0
StuPM <stuartpmcgee@gmail.com>
2023-04-13 16:37:33 +0100

    change business profile to use config variable

--- src/components/BusinessProfile/BusinessProfile.jsx
+++ src/components/BusinessProfile/BusinessProfile.jsx
@@ -14,6 +14,7 @@ import Email from "./Email";
 import PhoneNumber from "./PhoneNumber";
 import Details from "./Details";
 import axios from "axios";
+import { apiURL } from "../../config";
 
 const BusinessProfile = () => {
   const dispatch = useDispatch();
@@ -56,7 +57,7 @@ const BusinessProfile = () => {
       dispatch(editedBusinessData(businessData));
       setIsEdit(false);
     }
-    axios.post("http://localhost:6001/updateBusiness", businessData);
+    axios.post(`${apiURL}/updateBusiness`, businessData);
   };
 
   const cancelClick = () => {
